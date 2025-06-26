a110d1213dbc523d382f591b9d2c7bdae655c22c
Hessam Mahdiabadi <h.mahdiabadi@bluteam.ir>
2025-06-26 19:29:29 +0330

    Initial Commit

---
+++ TasksHub/TasksHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksHubApp.swift
+//  TasksHub
+//
+//  Created by HEssam on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct TasksHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TasksHubTests/TasksHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  TasksHubTests.swift
+//  TasksHubTests
+//
+//  Created by HEssam on 6/26/25.
+//
+
+import Testing
+@testable import TasksHub
+
+struct TasksHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
